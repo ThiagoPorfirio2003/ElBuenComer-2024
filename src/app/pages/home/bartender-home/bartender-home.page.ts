e0afb94796a470aa00d193be7f6f37a4f647a7d7
WilsonHuallpa <huallpa94canning@gmail.com>
2024-06-26 01:02:36 -0300

    desarrollo del listado de pedidos de bartender

--- src/app/pages/home/bartender-home/bartender-home.page.ts
+++ src/app/pages/home/bartender-home/bartender-home.page.ts
@@ -30,11 +30,11 @@ export class BartenderHomePage {
         },
         {
           id: '2',
-          name: 'Gaseosa',
-          description: 'Coca cola',
+          name: 'hamburguesa',
+          description: 'comida ',
           time: 10,
           price: 5,
-          isFood: false,
+          isFood: true,
           photoUrl: [
             'https://firebasestorage.googleapis.com/v0/b/elbuencomer-dff17.appspot.com/o/img%2Fproducts%2Fcostilla%20de%20cerdo0.1719015681138?alt=media&token=4c73e4e2-ec12-4384-a4a2-50699cb57983',
           ],
@@ -69,7 +69,7 @@ export class BartenderHomePage {
           description: 'papas fritas',
           time: 15,
           price: 8,
-          isFood: false,
+          isFood: true,
           photoUrl: [
             'https://firebasestorage.googleapis.com/v0/b/elbuencomer-dff17.appspot.com/o/img%2Fproducts%2Fcostilla%20de%20cerdo0.1719015681138?alt=media&token=4c73e4e2-ec12-4384-a4a2-50699cb57983',
           ],
@@ -79,7 +79,7 @@ export class BartenderHomePage {
       creationTime: Date.now(),
       price: 8,
       state: orderState.InPreparation,
-      barFinished: false,
+      barFinished: true,
       kitchenFinished: false,
     },
     {
@@ -102,29 +102,31 @@ export class BartenderHomePage {
       creationTime: Date.now(),
       price: 3,
       state: orderState.InPreparation,
-      barFinished: true,
+      barFinished: false,
       kitchenFinished: true,
     },
   ];
   public orders: Array<any> = [];
   constructor(private firebase: DataBaseService, private utils: UtilsService) {
-    //testear con firebase
+    // this.ordersAux.forEach(order => {
+    //   this.firebase.saveData(enumCollectionNames.Orders, order)
+    //     .catch((error) => {
+    //       let mensaje = this.utils.translateAuthError(error.code);
+    //       this.utils.showSweet({
+    //         titleText: mensaje.title,
+    //         text: mensaje.content,
+    //         icon: 'error',
+    //       });
+    //     });
+    // });
     this.firebase
       .getObservable(enumCollectionNames.Orders)
       .subscribe((pedidos) => {
         let pedidosAux: order[] = pedidos as order[];
         this.orders = [];
         pedidosAux.forEach((item) => {
-          if (item.state == orderState.InPreparation) {
-            const nonFoodProducts = item.products.filter(
-              (product) => !product.isFood
-            );
-            if (nonFoodProducts.length > 0) {
-              this.orders.push({
-                ...item,
-                products: nonFoodProducts,
-              });
-            }
+          if (item.state == orderState.InPreparation && !item.barFinished) {
+            this.orders.push(item);
           }
         });
         const mensaje = `Se ha añadido un nuevo pedido a la lista de espera en preparacion`;
@@ -135,7 +137,7 @@ export class BartenderHomePage {
   finalizeOrder(order: order) {
     this.utils
       .showSweet({
-        title: 'AFinalización de Pedido',
+        title: 'Finalización de Pedido',
         text:'¿Desea confirmar la finalización del pedido para la mesa ' + order.numberTable.toString() + '?',
         showCancelButton: true,
         cancelButtonText: 'No, aún no está listo',
@@ -147,22 +149,21 @@ export class BartenderHomePage {
             spinner: 'circular',
           });
           await loading.present();
-          // table.idCurrentClient = this.client.id;
-          // table.state = enumTableState.Reserved;
-          // this.firebase.saveData(enumCollectionNames.Tables,table,table.number.toString())
-          //   .then(() => {
-          //     //this.firebase.deleteData(enumCollectionNames.WaitingRoom, this.client.id);
-          //     loading.dismiss();
-          //     this.modalController.dismiss();
-          //   })
-          //   .catch((error) => {
-          //     let mensaje = this.utils.translateAuthError(error.code);
-          //     this.utils.showSweet({
-          //       titleText: mensaje.title,
-          //       text: mensaje.content,
-          //       icon: 'error',
-          //     });
-          //   });
+          order.barFinished = true;
+          if(order.kitchenFinished == true && order.barFinished == true){
+            order.state = orderState.Finished;
+          }
+          this.firebase.updateData(enumCollectionNames.Orders,order, order.id)
+          .then(() => {
+            loading.dismiss();
+          }).catch((error) => {
+              let mensaje = this.utils.translateAuthError(error.code);
+              this.utils.showSweet({
+                titleText: mensaje.title,
+                text: mensaje.content,
+                icon: 'error',
+              });
+            });
         }
       });
   }
