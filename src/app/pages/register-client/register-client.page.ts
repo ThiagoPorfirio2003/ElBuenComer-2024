37c06a0d97348be86a37eaf3fb565a5097f9927e
WilsonHuallpa <huallpa94canning@gmail.com>
2024-06-27 19:35:52 -0300

    fix del componente de camera

--- src/app/pages/register-client/register-client.page.ts
+++ src/app/pages/register-client/register-client.page.ts
@@ -56,6 +56,10 @@ export class RegisterClientPage {
     this.imageObject = data;
   }
 
+  capturarForm(data:FormGroup ){
+    this.anonymous = data;
+  }
+
   validateDni(control: AbstractControl): ValidationErrors | null {
     const value = control.value;
     const isValid = /^\d{8}$/.test(value);
